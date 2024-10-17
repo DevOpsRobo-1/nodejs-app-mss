2c94101cc96eb44ba0eb08ed5795906b9776857f
rami0123 <53289774+rami0123@users.noreply.github.com>
2024-10-17 21:29:29 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.31.224:9000/',
+  serverUrl: 'http://10.42.1.28:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_71817690eef2384cc9378677a0eb06d4c6734578',
+	    'sonar.login': 'squ_ea152dc8845c7739076057aa9c1a45ae5ca35218',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
